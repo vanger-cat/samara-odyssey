d37343ed4c8e453d31cb53a0627055194f97217e
Anton Katkov <katkov.anton@gmail.com>
2012-10-17 09:10:49 +0400

    added ChildBrowser Plugin for iOS

---
+++ Odyssey/Plugins/ChildBrowser/ChildBrowserCommand.m
@@ -0,0 +1,71 @@
+//  Created by Jesse MacFadyen on 10-05-29.
+//  Copyright 2010 Nitobi. All rights reserved.
+//  Copyright 2012, Randy McMillan
+// Continued maintainance @RandyMcMillan 2010/2011/2012
+
+#import "ChildBrowserCommand.h"
+
+#import <Cordova/CDVViewController.h>
+
+
+@implementation ChildBrowserCommand
+
+@synthesize childBrowser;
+
+- (void) showWebPage:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options // args: url
+{	
+	if(childBrowser == NULL)
+	{
+		childBrowser = [[ ChildBrowserViewController alloc ] initWithScale:FALSE ];
+		childBrowser.delegate = self;
+	}
+	
+/* // TODO: Work in progress
+	NSString* strOrientations = [ options objectForKey:@"supportedOrientations"];
+	NSArray* supportedOrientations = [strOrientations componentsSeparatedByString:@","];
+*/
+    CDVViewController* cont = (CDVViewController*)[ super viewController ];
+//	childBrowser.supportedOrientations = cont.supportedOrientations;
+	[ cont presentModalViewController:childBrowser animated:YES ];
+    
+	NSString *url = (NSString*) [arguments objectAtIndex:0];
+	
+
+	[childBrowser loadURL:url  ];
+
+}
+
+-(void) close:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options // args: url
+{
+	[ childBrowser closeBrowser];
+	
+}
+
+-(void) onClose
+{
+	NSString* jsCallback = [NSString stringWithFormat:@"ChildBrowser._onClose();",@""];
+	[self.webView stringByEvaluatingJavaScriptFromString:jsCallback];
+}
+
+-(void) onOpenInSafari
+{
+	NSString* jsCallback = [NSString stringWithFormat:@"ChildBrowser._onOpenExternal();",@""];
+	[self.webView stringByEvaluatingJavaScriptFromString:jsCallback];
+}
+
+
+-(void) onChildLocationChange:(NSString*)newLoc
+{
+	
+	NSString* tempLoc = [NSString stringWithFormat:@"%@",newLoc];
+	NSString* encUrl = [tempLoc stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+	 
+	NSString* jsCallback = [NSString stringWithFormat:@"ChildBrowser._onLocationChange('%@');",encUrl];
+	[self.webView stringByEvaluatingJavaScriptFromString:jsCallback];
+
+}
+
+
+
+
+@end
